790afd5342906b2d56015462cbb05924251bc369
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2025-10-20 11:47:08 +0300

    add Skeletonizer to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -8,6 +8,7 @@ import 'package:otex_app_task/features/home/presentation/views/widgets/custom_ho
 import 'package:otex_app_task/features/home/presentation/views/widgets/free_shipping_item.dart';
 import 'package:otex_app_task/features/home/presentation/views/widgets/product_category_list_view.dart';
 import 'package:otex_app_task/features/home/presentation/views/widgets/products_grid_view.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
@@ -17,41 +18,55 @@ class HomeViewBody extends StatefulWidget {
 }
 
 class _HomeViewBodyState extends State<HomeViewBody> {
+  bool isLoading = true;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeCubit,HomeState>(
+    return BlocConsumer<HomeCubit,HomeState>(
+      listener: (BuildContext context, state){
+        if(state is StartSaveDataState || state is StartLoadDataFromDBState){
+          isLoading = true;
+        }else{
+          isLoading = false;
+        }
+      },
       builder: (BuildContext context, state) {
         final cubit = HomeCubit.get(context);
-        return state is StartSaveDataState || state is StartLoadDataFromDBState
-        ? Center(child: CircularProgressIndicator(
-          color: kPrimaryColor,
-        ))
-        : SingleChildScrollView(
-          child: Column(
-            children: [
-              const CustomHomeHeader(),
-              CustomHomeChipChoice(
-                categories: cubit.categories,
-                tag: cubit.categoryTag,
-                onChanged: (newValue) {
-                  cubit.updateCategory(newValue);
-                },
-              ),
-              const SizedBox(height: 32),
-              ProductCategoryListView(homeCubit: cubit),
-              const SizedBox(height: 32),
-              cubit.isFreeShipping == true
-                  ? Column(
-                children: [
-                  const FreeShippingItem(),
-                  const SizedBox(height: 20),
-                ],
-              )
-                  : const SizedBox(),
+        return Skeletonizer(
+         textBoneBorderRadius: TextBoneBorderRadius(BorderRadius.circular(4)),
+          enabled: isLoading,
+          effect: ShimmerEffect(
+            baseColor: kBorderColor,
+            highlightColor: kGreyColor,
+            duration: const Duration(seconds: 2),
+          ),
+          containersColor: kWhiteColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const CustomHomeHeader(),
+                CustomHomeChipChoice(
+                  categories: cubit.categories,
+                  tag: cubit.categoryTag,
+                  onChanged: (newValue) {
+                    cubit.updateCategory(newValue);
+                  },
+                ),
+                const SizedBox(height: 32),
+                ProductCategoryListView(homeCubit: cubit),
+                const SizedBox(height: 32),
+                cubit.isFreeShipping == true
+                    ? Column(
+                  children: [
+                    const FreeShippingItem(),
+                    const SizedBox(height: 20),
+                  ],
+                )
+                    : const SizedBox(),
 
-              ProductsGridView(homeCubit: cubit),
-            ],
+                ProductsGridView(homeCubit: cubit),
+              ],
+            ),
           ),
         );
       },
