2267f7b45356c8c9aca65b6e8ed2fe7baa20e29c
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2025-10-20 13:25:51 +0300

    update plans list view

--- lib/features/plans/presentation/views/widgets/plans_list_view.dart
+++ lib/features/plans/presentation/views/widgets/plans_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:otex_app_task/features/plans/presentation/manager/plans_cubit/plans_cubit.dart';
 import 'package:otex_app_task/features/plans/presentation/views/widgets/plan_item.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class PlansListView extends StatelessWidget {
   const PlansListView({
@@ -19,22 +20,24 @@ class PlansListView extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 12),
       itemBuilder: (context, index) {
         final plan = cubit.plans[index];
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 32),
-          child: PlanItem(
-            isHealthPin: plan.isHealthPin ?? false,
-            numberOfDaysMoveUp: plan.numberOfDaysMoveUp ?? 0,
-            numberOfAdValidity: plan.numberOfAdValidity ?? 0,
-            isGlobal: plan.isGlobal ?? false,
-            isPremium: plan.isPremium ?? false,
-            planName: plan.planName,
-            planPrice: plan.planPrice.toString(),
-            checkBoxValue: plan.isSelected,
-            flagText: plan.flagText,
-            numberOfViews: plan.numberOfViews,
-            onChanged: (newValue) {
-              cubit.updatePlanCheckBox(plan.id!, newValue ?? false);
-            },
+        return Skeleton.leaf(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: PlanItem(
+              isHealthPin: plan.isHealthPin ?? false,
+              numberOfDaysMoveUp: plan.numberOfDaysMoveUp ?? 0,
+              numberOfAdValidity: plan.numberOfAdValidity ?? 0,
+              isGlobal: plan.isGlobal ?? false,
+              isPremium: plan.isPremium ?? false,
+              planName: plan.planName,
+              planPrice: plan.planPrice.toString(),
+              checkBoxValue: plan.isSelected,
+              flagText: plan.flagText,
+              numberOfViews: plan.numberOfViews,
+              onChanged: (newValue) {
+                cubit.updatePlanCheckBox(plan.id!, newValue ?? false);
+              },
+            ),
           ),
         );
       },
